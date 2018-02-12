c93335d09540196994b18c149f310b18d1a8f72b
2youngjae <35296059+2youngjae@users.noreply.github.com>
2018-02-12 14:55:30 +0900

    Update 14890 경사로.cpp

--- 14890 경사로.cpp
+++ 14890 경사로.cpp
@@ -1,94 +1,83 @@
 #include <stdio.h>
 #include <memory.h>
- 
-int n, l, res;
-int map[100][100];
+
+int map[102][102];
 bool visited[100];
- 
-void isVertical(int r, int c) {
-    if (r == n - 1) {
-        res++;
-        return;
-    }
- 
-    if (map[r][c] == map[r+1][c]) {
-        isVertical(r+1, c);
-    }
-    else if (map[r][c] == map[r+1][c] - 1) {
-        if (r - l + 1 < 0) {
-            return;
-        }
-        for (int i = 0; i < l; i++) {
-            if (!visited[r - i]) {
-                visited[r - i] = true;
-            }
-            else return;
-        }
-        isVertical(r+1, c);
-    }
-    else if (map[r][c] == map[r+1][c] + 1) {
-        if (r + l > n - 1) {
-            return;
-        }
-        for (int i = 1; i <= l; i++) {
-            if (!visited[r + i] && map[r][c]==map[r + i][c] + 1) {
-                visited[r + i] = true;
-            }
-            else return;
-        }
-        isVertical(r+l, c);
-    }
+int n, l, res;
+
+void f1(int r,int c) {
+	if (map[r][c + 1] == 0) {
+		res++;
+		return;
+	}
+	
+	//다음칸과 높이가 같을때
+	if (map[r][c] == map[r][c + 1]) {
+		f1(r, c + 1);
+	}
+	//다음칸보다 높이가 높을때
+	else if (map[r][c] - 1 == map[r][c + 1] ) {
+		if (c + l > n) return;
+		for (int i = 1; i <= l; i++) {
+			if (map[r][c] - 1 != map[r][c + i]) return;
+			visited[c + i] = true;
+		}
+		f1(r, c + 1);
+	}
+	//다음칸보다 높이가 낮을때
+	else if (map[r][c] + 1 == map[r][c + 1]) {
+		if (c - l + 1 < 1)return;
+		for (int i = 0; i < l; i++) {
+			if (visited[c - i] || (map[r][c] != map[r][c-i])) return;
+			visited[c - i] = true;
+		}
+		f1(r, c + 1);
+	}
 }
- 
-void isHorizontal(int r,int c) {
-    if (c == n - 1) {
-        res++;
-        return;
-    }
-    
-    if (map[r][c] == map[r][c + 1]) {
-        isHorizontal(r, c + 1);
-    }
-    else if (map[r][c] == map[r][c + 1] - 1) {
-        if (c - l + 1 < 0) {
-            return;
-        }
-        for (int i = 0; i < l; i++) {
-            if (!visited[c-i]) {
-                visited[c-i] = true;
-            }
-            else return;
-        }
-        isHorizontal(r, c + 1);
-    }
-    else if (map[r][c] == map[r][c + 1] + 1) {
-        if (c + l > n - 1) {
-            return;
-        }
-        for (int i = 1; i <= l; i++) {
-            if (!visited[c + i] && map[r][c] == map[r][c + i] + 1) {
-                visited[c + i] = true;
-            }
-            else return;
-        }
-        isHorizontal(r, c + l);
-    }
+
+void f2(int r, int c) {
+	if (map[r+1][c] == 0) {
+		res++;
+		return;
+	}
+
+	//다음칸과 높이가 같을때
+	if (map[r][c] == map[r+1][c ]) {
+		f2(r+1, c);
+	}
+	//다음칸보다 높이가 높을때
+	else if (map[r][c] - 1 == map[r+1][c]) {
+		if (r + l > n) return;
+		for (int i = 1; i <= l; i++) {
+			if (map[r][c] - 1 != map[r + i][c]) return;
+			visited[r + i] = true;
+		}
+		f2(r+1, c);
+	}
+	//다음칸보다 높이가 낮을때
+	else if (map[r][c] + 1 == map[r+1][c]) {
+		if (r - l + 1 < 1)return;
+		for (int i = 0; i < l; i++) {
+			if (visited[r - i] || (map[r][c] != map[r-i][c])) return;
+			visited[r - i] = true;
+		}
+		f2(r+1, c);
+	}
 }
- 
+
 int main() {
-    scanf("%d %d", &n, &l);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &map[i][j]);
-        }
-    }
- 
-    for (int i = 0; i < n; i++) {
-        memset(visited, 0, sizeof(visited));
-        isVertical(0, i);
-        memset(visited, 0, sizeof(visited));
-        isHorizontal(i, 0);
-    }
- 
-    printf("%d\n", res);
+	scanf("%d %d", &n, &l);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			scanf("%d", &map[i][j]);
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		memset(visited, 0, sizeof(visited));
+		f1(i, 1);
+		memset(visited, 0, sizeof(visited));
+		f2(1, i);
+	}
+	printf("%d\n",res);
 }
