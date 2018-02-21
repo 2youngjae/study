c1cf6523f8e004ed95ee8a50f62468fcc013fbf4
2youngjae <35296059+2youngjae@users.noreply.github.com>
2018-02-21 18:34:39 +0900

    Update 백준 3190 뱀.cpp

--- 백준 3190 뱀.cpp
+++ 백준 3190 뱀.cpp
@@ -1,6 +1,8 @@
 /*
-
-1. 방향에 대한 정보를 dir 에 저장하고 방향전환함수 turn을 만들어, 주어진 시간에 대해 방향전환이 필요할 경우 방향전환 시킴
+*map[x][y] == 0 : 일반땅
+           == 1 : 사과
+           == 2 : 뱀
+1. 방향에대한 정보를 dir 에 저장하고 방향전환함수 turn을 만들어, 주어진 시간에 대해 방향전환이 필요할 경우 방향전환 시킴
 
 2. 방향에 따라 x,y값 변화 시킴
 
@@ -9,11 +11,9 @@
 4. 사과를 먹지 않았을경우 뒤쪽부터 한칸씩 땡겨준다.
 
 5. 벽에 부딪히거나 몸통에 부딪히면 종료
-
 */
 
 #include <iostream>
-#include <queue>
 using namespace std;
 
 //x,y좌표 저장용 구조체
@@ -22,11 +22,12 @@ struct Node {
 	int y;
 };
 
-//방향정보를  구조체
+//방향전환정보를 구조화시킨 구조체
 struct Dir {
 	int time;
 	char c;
-	Dir(int _t,char _c) {
+	Dir() {}
+	Dir(int _t, char _c) {
 		time = _t;
 		c = _c;
 	}
@@ -43,10 +44,12 @@ void turn(int &dir, int c) {
 	}
 }
 
-int n, k, l, snake_dir, idx;
+int n, k, l, snake_dir, idx, info_idx;
 int map[102][102];
 Node snake[10001];
-queue<Dir> dirInfo;
+Dir info[100];
+int dx[] = { 0,1,0,-1 };
+int dy[] = { 1,0,-1,0 };
 
 int main() {
 	scanf("%d%d", &n, &k);
@@ -60,45 +63,32 @@ int main() {
 		int x;
 		char c;
 		scanf("%d %c", &x, &c);
-		dirInfo.push(Dir(x, c));
+		info[info_idx++] = Dir(x, c);
 	}
 
 	// 정보들 초기화
 	int time = 0;
+	int info_i = 0;
 	idx = 10000;
 	snake[idx].x = 1;
 	snake[idx].y = 1;
+	map[1][1] = 2;
 
 	while (true) {
 		//방향 전환
-		if (!dirInfo.empty()) {
-			if (time == dirInfo.front().time) {
-				turn(snake_dir, dirInfo.front().c);
-				dirInfo.pop();
+		if (info_i != info_idx) {
+			if (time == info[info_i].time) {
+				turn(snake_dir, info[info_i++].c);
 			}
 		}
+
 		time++;
 
 		//뱀 이동
-		// 0동,1남,2서,3북
 		//머리 좌표 이동
 		int x, y;
-		if (snake_dir == 0) {
-			x = snake[idx].x;
-			y = snake[idx].y + 1;
-		}
-		else if (snake_dir == 1) {
-			x = snake[idx].x + 1;
-			y = snake[idx].y;
-		}
-		else if (snake_dir == 2) {
-			x = snake[idx].x;
-			y = snake[idx].y - 1;
-		}
-		else {
-			x = snake[idx].x - 1;
-			y = snake[idx].y;
-		}
+		x = snake[idx].x + dx[snake_dir];
+		y = snake[idx].y + dy[snake_dir];
 
 		//종료조건
 		//벽에 부딪히면
@@ -107,14 +97,7 @@ int main() {
 			break;
 		}
 		//몸통에 부딪히면
-		bool flag = false;
-		for (int i = idx; i <= 10000; i++) {
-			if (x == snake[i].x && y == snake[i].y) {
-				flag = true;
-				break;
-			}
-		}
-		if (flag) break;
+		if (map[x][y] == 2) break;
 
 		//사과가 있을경우 몸통 당기지 않고
 		//머리 위치 유지
@@ -122,16 +105,18 @@ int main() {
 			idx--;
 			snake[idx].x = x;
 			snake[idx].y = y;
-			map[x][y] = 0;
+			map[x][y] = 2;
 		}
 		//사과가 없을경우 몸통 당기기
 		else {
+			map[snake[10000].x][snake[10000].y] = 0;
 			for (int i = 10000; i > idx; i--) {
 				snake[i].x = snake[i - 1].x;
 				snake[i].y = snake[i - 1].y;
 			}
 			snake[idx].x = x;
 			snake[idx].y = y;
+			map[x][y] = 2;
 		}
 	}
 	printf("%d\n", time);
